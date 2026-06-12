d809a7b3b859792e016832d3cab3050221de7156
Joan Antoni Torradell Payeras <joanantonitorrandell@paucasesnovescifp.cat>
2026-06-13 00:22:44 +0200

    fff

--- front/src/lib/auth-client.ts
+++ front/src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
 
-const baseURL = process.env.NEXT_PUBLIC_BETTER_AUTH_URL || 'http://localhost:4000';
+const baseURL = process.env.NEXT_PUBLIC_BETTER_AUTH_URL || '';
 
 export const authClient = createAuthClient({
   baseURL,
