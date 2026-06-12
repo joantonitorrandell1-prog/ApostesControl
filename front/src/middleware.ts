10808258603f21c862b7915dc317f5522c318430
Joan Antoni Torradell Payeras <joanantonitorrandell@paucasesnovescifp.cat>
2026-06-13 00:15:26 +0200

    fix s

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -2,6 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith('/api')) {
+    return NextResponse.next();
+  }
+
   return NextResponse.next();
 }
 
